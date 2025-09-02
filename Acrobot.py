05da8a877de6ed431388dd183e5ae04b62a6a7fd
Jasper <jaspervandenberg250@gmail.com>
2025-09-02 06:48:49 +0000

    working acrobot linearized feedback controller, interactive window

--- Acrobot.py
+++ Acrobot.py
@@ -11,11 +11,12 @@ from pydrake.all import (
     Parser,
     Simulator,
     StartMeshcat,
+    RegionOfAttraction
 )
 
 
-meshcat=StartMeshcat()
-
+meshcat=StartMeshcat() #visualizing outside of notebook
+print(f"Open the following URL in your browser to see the visualizer: {meshcat.web_url()}")
 
 builder = DiagramBuilder()
 acrobot, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
@@ -43,24 +44,23 @@ def acrobot_balancing(xequilibrium, Q, R):
     lqr.set_name("LQR")
 
     builder.Connect(acrobot.get_state_output_port(), lqr.get_input_port(0)) 
-    builder.Connect(lqr.get_output_port(0), acrobot.get_actuation_input_port())
+    builder.Connect(lqr.get_output_port(0), acrobot.get_actuation_input_port()) # nan atr actuation input port?
 
     actuation_logger = LogVectorOutput(lqr.get_output_port(), builder)
     state_logger = LogVectorOutput(acrobot.get_state_output_port(), builder)
     visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
-    visualizer.set_name("visualizer")
+    visualizer.set_name("visualizer2")
 
     diagram = builder.Build()
-
-    return diagram, actuation_logger, state_logger
+    return diagram, context, actuation_logger, state_logger 
 
 
 def simulate_and_animate(diagram, xstart, alogger, slogger, sim_time=5.0, visualize=True):
     simulator = Simulator(diagram)
     simulator.set_publish_every_time_step(False)
-    simulator.get_mutable_integrator().set_fixed_step_mode(True)
+    simulator.get_mutable_integrator().set_fixed_step_mode(True) #RK3 by default
 
-    visualizer=diagram.GetSubsystemByName("visualizer")
+    visualizer=diagram.GetSubsystemByName("visualizer2")
     visualizer.StartRecording(False)
 
     if len(xstart) != diagram.GetSubsystemByName("Acrobot").num_continuous_states():
@@ -86,9 +86,26 @@ def simulate_and_animate(diagram, xstart, alogger, slogger, sim_time=5.0, visual
     return alog, slog
 
 # LQR for double pendulum balancing
-Q = np.diag([10.0, 10.0, 0.5, 0.5])  # State cost matrix
+Q = np.diag([10.0, 10.0, 1, 1])  # State cost matrix
 R = np.eye(1)
-xstart = np.array([np.pi, -np.pi/2, 0.0, 0.0])  # Initial state: [theta1, theta2, theta1_dot, theta2_dot]
-diagram, actuation_logger, state_logger = acrobot_balancing(xequilibrium, Q, R)
+xstart = np.array([0.99*np.pi, 0.0, 0.0, 0.0])  # Initial state: [theta1, theta2, theta1_dot, theta2_dot], LQR can only handle small deviations from equilibrium
+diagram, context, actuation_logger, state_logger = acrobot_balancing(xequilibrium, Q, R)
 
-plot_system_graphviz(diagram)
+
+a_log, s_log = simulate_and_animate(diagram, xstart, actuation_logger, state_logger, sim_time=6.0, visualize=True)
+#log.sample_times()
+u=a_log.data()
+x=s_log.data()
+print(u.shape)
+print(x.shape)
+for i in range(10):
+    print(u[:,i])
+    #print(x[:,-i])
+print(x[:,-1]) #initial state
+
+# roa = RegionOfAttraction(acrobot, context)
+# print(f"Estimated ROA volume: {roa.volume():.4f}") 
+# non-unique solution gives error, study Lyapunov functions and ROA before attempting this again
+
+# For Visualization you need to run in interatcive window...
+
